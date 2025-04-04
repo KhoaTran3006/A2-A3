99b906e6554c4a1c0ee3f0255971ab4bfa3a6306
KhoaTran3006 <dangkhoamt302@gmail.com>
2025-04-05 00:53:51 +0700

    Dev 04/04
    Fade in out trigger by mouse click added
    can implement with the cam for example if bool CamOnHand is true start the coroutine

---
+++ A2 A3/Assets/Scenes/FadeInFadeOut.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInFadeOut : MonoBehaviour
+{
+    public CanvasGroup fadeCanvasGroup;
+    public float fadeDuration = 1f;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            StartCoroutine(FadeIn());
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            StartCoroutine(FadeOut());
+        }
+        
+    }
+
+    public IEnumerator FadeIn()
+    {
+        float time = fadeDuration;
+        while (time > 0f)
+        {
+            time -= Time.deltaTime;
+            fadeCanvasGroup.alpha = time / fadeDuration;
+            yield return null;
+        }
+        fadeCanvasGroup.alpha = 0f;
+        fadeCanvasGroup.blocksRaycasts = false;
+    }
+
+    public IEnumerator FadeOut()
+    {
+        fadeCanvasGroup.blocksRaycasts = true;
+        float time = 0f;
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+            fadeCanvasGroup.alpha = time / fadeDuration;
+            yield return null;
+        }
+        fadeCanvasGroup.alpha = 1f;
+    }
+
+}
