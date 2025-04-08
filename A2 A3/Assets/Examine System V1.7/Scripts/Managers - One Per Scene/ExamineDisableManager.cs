d726f7e533cc3c9e16ba441840f2c8cdefc7566e
Khoa Tran Dang <dangkhoamt302@gmail.com>
2025-04-08 16:06:21 +0700

    DevLog 08/04
    Inspection added

---
+++ A2 A3/Assets/Examine System V1.7/Scripts/Managers - One Per Scene/ExamineDisableManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityStandardAssets.ImageEffects;
+
+namespace ExamineSystem
+{
+    public class ExamineDisableManager : MonoBehaviour
+    {
+        [SerializeField] private ExamineInteractor interactorScript;
+        [SerializeField] private MonoBehaviour player;
+        [SerializeField] private BlurOptimized blur;
+
+        [Header("Should persist?")]
+        [SerializeField] private bool persistAcrossScenes = true;
+
+        public static ExamineDisableManager instance;
+
+        void Awake()
+        {
+            
+            if (instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = this;
+                if (persistAcrossScenes)
+                {
+                    DontDestroyOnLoad(gameObject);
+                }
+            }
+        }
+
+        public void DisablePlayer(bool disable)
+        {
+            if (disable)
+            {
+                if(player != null)
+                {
+                    player.enabled = false;
+                }
+                else
+                {
+                    print("Disable Manager: You will need to add the included player character here but if you have your own, you will need to change the reference");
+                }
+
+                interactorScript.enabled = false;
+
+                blur.enabled = true;
+                ExamineUIManager.instance.EnableCrosshair(false);
+
+            }
+            else
+            {
+                if (player != null) 
+                {
+                    player.enabled = true;
+                }
+                else 
+                {
+                    print("Disable Manager: You will need to add the included player character here but if you have your own, you will need to change the reference");
+                }
+
+                interactorScript.enabled = true;
+
+                blur.enabled = false;
+                ExamineUIManager.instance.EnableCrosshair(true);
+            }
+        }
+    }
+}
